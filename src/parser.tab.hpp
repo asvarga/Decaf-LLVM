52ae3dcd848176e68e61350892eb6e58c80685cc
denizalp <denizalp.goktas@outlook.com>
2019-11-05 01:40:11 -0500

    makefile changes + ast files added

---
+++ src/parser.tab.hpp
@@ -0,0 +1,115 @@
+/* A Bison parser, made by GNU Bison 3.0.4.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_YY_PARSER_TAB_HPP_INCLUDED
+# define YY_YY_PARSER_TAB_HPP_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    BREAK = 258,
+    CLASS = 259,
+    CONTINUE = 260,
+    ELSE = 261,
+    EXTENDS = 262,
+    IF = 263,
+    NEW = 264,
+    PRIVATE = 265,
+    PROTECTED = 266,
+    PUBLIC = 267,
+    RETURN = 268,
+    STATIC = 269,
+    SUPER = 270,
+    TYPE_VOID = 271,
+    TYPE_INT = 272,
+    TYPE_BOOL = 273,
+    TYPE_CHAR = 274,
+    THIS = 275,
+    WHILE = 276,
+    FORBIDDEN = 277,
+    UNENDED_COM = 278,
+    LEQ = 279,
+    LESS = 280,
+    GREATER = 281,
+    EQEQ = 282,
+    GEQ = 283,
+    EQ = 284,
+    NEQ = 285,
+    LAND = 286,
+    LOR = 287,
+    ADD = 288,
+    SUBS = 289,
+    MULT = 290,
+    DIV = 291,
+    MOD = 292,
+    NEG = 293,
+    OP = 294,
+    CP = 295,
+    OB = 296,
+    CB = 297,
+    OC = 298,
+    CC = 299,
+    COMMA = 300,
+    PERIOD = 301,
+    IDENTIFIER = 302,
+    NILL = 303,
+    BOOLEAN = 304,
+    INTEGER = 305,
+    STRING = 306,
+    CHAR = 307,
+    UNRECOG_ERROR = 308,
+    UMINUS = 309
+  };
+#endif
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+int yyparse (void);
+
+#endif /* !YY_YY_PARSER_TAB_HPP_INCLUDED  */
